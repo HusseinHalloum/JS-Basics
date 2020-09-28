ec6fdcbef2bdbfc62864c07a510ad4456fec46a9
hussein <hseinhalloum00@gmail.com>
2020-09-28 12:09:47 +0300

    Basics JS 1 Step-5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,4 @@
+var number1 = prompt("Enter the 1st number :");
+var number2 = prompt("Enter the 2nd number :");
+var multiply = number1*number2;
+alert("the multiply of " + number1 +" and " + number2 + " is : " + multiply);
