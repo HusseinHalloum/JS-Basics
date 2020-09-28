99b1c665f8668b6892b75eb1cf3efb66324f1dc7
hussein <hseinhalloum00@gmail.com>
2020-09-28 11:41:22 +0300

    Basics JS 1 Step-4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("Enter Your Name");
+var surname = prompt("Enter Your Surname");
+var city = prompt("Enter Your City");
+alert("Hello " + name + " " + surname +" . " + "Your city is " + city);
