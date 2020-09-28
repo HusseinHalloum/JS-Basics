0c1eae62fbe064130ad18555484b904917cc9fca
hussein <hseinhalloum00@gmail.com>
2020-09-28 12:18:08 +0300

    Basics JS 1 Step-6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,4 @@
+var number1 = prompt("Enter the 1st number :");
+var number2 = prompt("Enter the 2nd number :");
+var remainder = number1%number2;
+alert("the remainder of the division is " + remainder);
