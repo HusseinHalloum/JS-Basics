6d9303e7a4e1d45513a2bf91d8982347c79cf178
hussein <hseinhalloum00@gmail.com>
2020-09-29 14:52:48 +0300

    Basics JS 2 Step-6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,13 @@
+var imgs = Array.from(document.querySelectorAll("img"));
+
+imgs.forEach((img, i) => {
+    img.addEventListener('mouseover', function () {
+      img.src = "images/image" + (i + 1) + "_2.jpg";
+    });
+  });
+
+  imgs.forEach((img, i) => {
+      img.addEventListener('mouseout', function () {
+        img.src = "images/image" + (i + 1) + ".jpg";
+      });
+    });
